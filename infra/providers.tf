0bc4f06dc9a8f4518286f3ddf50e3a421bc964e2
paulonye <nwosupaul141@gmail.com>
2024-01-19 15:37:58 +0100

    Added azurerm provider

--- infra/providers.tf
+++ infra/providers.tf
@@ -4,13 +4,17 @@ terraform {
       source  = "hashicorp/google"
       version = "5.10.0"
     }
-    prefect = {
-      source  = "PrefectHQ/prefect"
-      version = "0.1.1"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "5.31.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    prefect = {
+      source  = "PrefectHQ/prefect"
+      version = "0.1.1"
     }
   }
   backend "gcs" {
@@ -32,4 +36,13 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
+  tenant_id       = var.azurerm_tenant_id
+  subscription_id = var.azurerm_subscription_id
 }
